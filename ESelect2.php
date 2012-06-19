314995724edce5438b20c9dc41a1dbf77e55a2c0
unknown <User@.(none)>
2012-06-19 10:04:41 +0700

    Placeholder bug fix

--- ESelect2.php
+++ ESelect2.php
@@ -3,6 +3,7 @@
  * Wrapper for ivaynberg jQuery select2 (https://github.com/ivaynberg/select2)
  * 
  * @author Anggiajuang Patria <anggiaj@gmail.com>
+ * @license http://www.opensource.org/licenses/apache2.0.php
  */
 class ESelect2 extends CInputWidget
 {
@@ -18,9 +19,16 @@ class ESelect2 extends CInputWidget
    * @var integer Script position
    */
   public $scriptPosition=null;
+  
   public function run()
   {
     list($this->name,$this->id)=$this->resolveNameId();
+    
+    if(isset($this->htmlOptions['placeholder'])) 
+      $this->options['placeholder']=$this->htmlOptions['placeholder'];
+    $data=array();
+    if(isset($this->options['placeholder'])) $data['']='';
+    $this->data=$data+$this->data;
     if($this->hasModel())
     {
       echo CHtml::activeDropDownList($this->model,$this->attribute,$this->data,$this->htmlOptions);
@@ -31,11 +39,14 @@ class ESelect2 extends CInputWidget
       echo CHtml::dropDownList($this->name,$this->value,$this->data,$this->htmlOptions);
     }
     
-    $bu=Yii::app()->assetManager->publish(dirname(__FILE__).'/assets');
+    $bu=Yii::app()->assetManager->publish(dirname(__FILE__).'/assets/select2-2.1');
     $cs=Yii::app()->clientScript;
     $cs->registerCssFile($bu.'/select2.css');
+    
     if($this->scriptPosition===null) $this->scriptPosition=$cs->coreScriptPosition;
     $cs->registerScriptFile($bu.'/select2.min.js',$this->scriptPosition);
+    
+    
     $options=$this->options?CJavaScript::encode($this->options):'';
     $cs->registerScript(__CLASS__.'#'.$this->id,"$('#{$this->id}').select2({$options});");
   }
