b2c501eb7549779fb56fa3581aee07e969354154
anggiaj <anggiaj@gmail.com>
2012-08-21 09:44:46 +0700

    Terjemahan bahasa Indonesia

---
+++ messages/id/select2.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+	'No matches found' => 'Tidak ada has',
+	'Please enter {chars} more characters' => 'Mohon masukkan {chars} atau lebih karakter',
+	'You can only select {count} items' => 'Anda hanya dapat memilih {count} item',
+	'Loading more results...' => 'Memuat lebih banyak hasil...',
+	'Searching...' => 'Mencari...',
+);
