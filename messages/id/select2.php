10dfa1d649415510a99fb670bcd762efd87d5896
anggiaj <anggiaj@gmail.com>
2012-08-21 09:51:22 +0700

    Benerin terjemahan

--- messages/id/select2.php
+++ messages/id/select2.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-	'No matches found' => 'Tidak ada has',
+	'No matches found' => 'Tidak diketemukan hasil',
 	'Please enter {chars} more characters' => 'Mohon masukkan {chars} atau lebih karakter',
 	'You can only select {count} items' => 'Anda hanya dapat memilih {count} item',
 	'Loading more results...' => 'Memuat lebih banyak hasil...',
