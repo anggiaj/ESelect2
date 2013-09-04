047b458fb3ddc97d374495caf70c1c1266278115
Tom Niessink <tommybravo@hotmail.com>
2013-09-04 20:18:41 +0200

    Dutch translation added

---
+++ messages/nl/select2.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+	'No matches found' => 'Niets gevonden',
+	'Please enter {chars} more characters' => 'Nog {chars} extra karakter(s) vereist',
+	'Please enter {chars} less characters' => 'Nog {chars} karakter(s) te veel',
+	'You can only select {count} items' => 'Je kunt maar {count} items selecteren',
+	'Loading more results...' => 'Meer resultaten aan het laden...',
+	'Searching...' => 'Aan het zoeken...',
+);
