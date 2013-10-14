57972da543561820a3c590a6564c8f658e5c1177
gabrieljenik <gabriel.jenik@gmail.com>
2013-10-13 21:17:39 -0300

    Create select2.php
    Spanish translation

---
+++ messages/es/select2.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+	'No matches found' => 'No se han encontrado resultados',
+	'Please enter {chars} more characters' => 'Por favor, tipé {chars} caracteres más',
+	'You can only select {count} items' => 'Solo puede seleccionar {count} elementos',
+	'Loading more results...' => 'Cargando más resultados...',
+	'Searching...' => 'Buscando...',
+);
