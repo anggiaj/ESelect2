250543ecc87f32c08b5f51a196122459426adf7c
Yakir Sitbon <kingyes1@gmail.com>
2014-03-17 20:03:05 +0000

    added credit for translator

--- messages/he/select2.php
+++ messages/he/select2.php
@@ -1,9 +1,12 @@
 <?php
+/**
+ * @author Yakir Sitbon <http://www.yakirs.net/>
+ */
 return array(
-	'No matches found' => 'Niets gevonden',
-	'Please enter {chars} more characters' => 'Nog {chars} extra karakter(s) vereist',
-	'Please enter {chars} less characters' => 'Nog {chars} karakter(s) te veel',
-	'You can only select {count} items' => 'Je kunt maar {count} items selecteren',
-	'Loading more results...' => 'Meer resultaten aan het laden...',
-	'Searching...' => 'Aan het zoeken...',
+	'No matches found' => 'לא נמצאו תוצאות',
+	'Please enter {chars} more characters' => 'אנא הזן עוד {chars} תווים',
+	'Please enter {chars} less characters' => 'אנא מחק {chars} תווים',
+	'You can only select {count} items' => 'אתה יכול לצפות ב-{count} רשומות בלבד',
+	'Loading more results...' => 'טוען עוד תוצאות...',
+	'Searching...' => 'מחפש...',
 );
